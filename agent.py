fb98098b5d4f5f764f8e32c9ef67aeea8acd6fbe
Boombot01 <hugo.senetaire@eleves.enpc.fr>
2020-11-17 10:22:12 +0100

    fix random agent

--- agent.py
+++ agent.py
@@ -8,32 +8,37 @@ import matplotlib.pyplot as plt
 from kaggle_environments.envs.football.helpers import *
 
 @human_readable_agent
-def naive_agent(obs):
-    # Make sure player is running.
-    if Action.Sprint not in obs['sticky_actions']:
-        return Action.Sprint
-    # We always control left team (observations and actions
-    # are mirrored appropriately by the environment).
-    controlled_player_pos = obs['left_team'][obs['active']]
-    # Does the player we control have the ball?
-    if obs['ball_owned_player'] == obs['active'] and obs['ball_owned_team'] == 0:
-        # Shot if we are 'close' to the goal (based on 'x' coordinate).
-        if controlled_player_pos[0] > 0.5:
-            return Action.Shot
-        # Run towards the goal otherwise.
-        return Action.Right
-    else:
-        # Run towards the ball.
-        if obs['ball'][0] > controlled_player_pos[0] + 0.05:
+class NaiveAgent():
+    def __init__():
+        print("Random agent")
+
+
+    def get_action(obs):
+        # Make sure player is running.
+        if Action.Sprint not in obs['sticky_actions']:
+            return Action.Sprint
+        # We always control left team (observations and actions
+        # are mirrored appropriately by the environment).
+        controlled_player_pos = obs['left_team'][obs['active']]
+        # Does the player we control have the ball?
+        if obs['ball_owned_player'] == obs['active'] and obs['ball_owned_team'] == 0:
+            # Shot if we are 'close' to the goal (based on 'x' coordinate).
+            if controlled_player_pos[0] > 0.5:
+                return Action.Shot
+            # Run towards the goal otherwise.
             return Action.Right
-        if obs['ball'][0] < controlled_player_pos[0] - 0.05:
-            return Action.Left
-        if obs['ball'][1] > controlled_player_pos[1] + 0.05:
-            return Action.Bottom
-        if obs['ball'][1] < controlled_player_pos[1] - 0.05:
-            return Action.Top
-        # Try to take over the ball if close to the ball.
-        return Action.Slide
+        else:
+            # Run towards the ball.
+            if obs['ball'][0] > controlled_player_pos[0] + 0.05:
+                return Action.Right
+            if obs['ball'][0] < controlled_player_pos[0] - 0.05:
+                return Action.Left
+            if obs['ball'][1] > controlled_player_pos[1] + 0.05:
+                return Action.Bottom
+            if obs['ball'][1] < controlled_player_pos[1] - 0.05:
+                return Action.Top
+            # Try to take over the ball if close to the ball.
+            return Action.Slide
 
 
 class DFPAgent():
